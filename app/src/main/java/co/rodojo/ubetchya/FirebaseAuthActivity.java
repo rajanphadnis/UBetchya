d965185bad77aa303b0712b6702561b1690bc60c
Rajan Phadnis <rajansd28@gmail.com>
2018-01-01 14:58:09 -0800

    First Commit

---
+++ app/src/main/java/co/rodojo/ubetchya/FirebaseAuthActivity.java
@@ -0,0 +1,13 @@
+package co.rodojo.ubetchya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirebaseAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_auth);
+    }
+}
