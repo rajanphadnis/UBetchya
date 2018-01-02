5ad57be779d79ec62c7fed50b02941db63bbe32d
Rajan Phadnis <rajansd28@gmail.com>
2018-01-01 18:21:33 -0800

    basic bluetooth stuff

--- app/src/main/java/co/rodojo/ubetchya/MainActivity.java
+++ app/src/main/java/co/rodojo/ubetchya/MainActivity.java
@@ -1,16 +1,29 @@
 package co.rodojo.ubetchya;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static int REQUEST_BLUETOOTH = 1;
+    private String TAG = "General";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +37,38 @@ public class MainActivity extends AppCompatActivity {
 //
 //            }
 //        });
+        BluetoothAdapter BTAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Phone does not support Bluetooth so let the user know and exit.
+        if (BTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        else {
+            if (!BTAdapter.isEnabled()) {
+                Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+                Log.e(TAG, "Bluetooth On");
+            }
+            IntentFilter filter = new IntentFilter();
+            Log.e(TAG, "Intent Filter On");
+            filter.addAction(BluetoothDevice.ACTION_FOUND);
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+            filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+            registerReceiver(mReceiver, filter);
+            Log.e(TAG, "Receiver Started");
+            BTAdapter.startDiscovery();
+            Log.e(TAG, "Discovery On");
+
+        }
     }
 
     @Override
@@ -46,5 +91,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                rep("started");
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                rep("finished");
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                rep("found");
+                LinearLayout ll = (LinearLayout) findViewById(R.id.mainline);
+                TextView tv = new TextView(MainActivity.this);
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                tv.setText(device.getName());
+                ll.addView(tv);
+                //Toast.makeText(MainActivity.this, "Found device " + device.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    private void rep(String m) {
+        Log.e("TAG", m);
+    }
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
